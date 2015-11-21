61becf30a8309e382d275a10b9e3df0eb440dd33
Franche van den Berg <franche.vandenberg@gmail.com>
2015-11-21 14:16:48 +0200

    Added Initial Project Files

---
+++ TTT_CodeHouse/TTT_Server/Lib/TcpHandler.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TTT_Server.Lib
+{
+    internal class TcpHandler
+    {
+        public static void Receive(int portNumber, string ipAddress)
+        {
+            TcpListener server = null;
+            try
+            {
+                var port = portNumber;
+                var localAddr = IPAddress.Parse(ipAddress);
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                var bytes = new byte[32];
+                string data;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    var client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    // Get a stream object for reading and writing
+                    var stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        var msg = Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                if (server != null) server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+        public static void Send(int portNumber, string ipAddress, string message)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                var port = portNumber;
+                var client = new TcpClient(ipAddress, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                var data = Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                var stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new byte[32];
+
+                // String to store the response ASCII representation.
+
+                // Read the first batch of the TcpServer response bytes.
+                var bytes = stream.Read(data, 0, data.Length);
+                var responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ TTT_CodeHouse/TTT_Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TTT_Server.Lib;
+
+namespace TTT_Server
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Press 1 for Server, 2 for Client");
+            var pressed = Console.ReadKey();
+            Console.Clear();
+            if (pressed.Key == ConsoleKey.D1)
+                TcpHandler.Receive(13000, "127.0.0.1");
+            else
+            {
+                Console.Write("Type a Message: ");
+                var message = Console.ReadLine();
+                Console.WriteLine();
+                TcpHandler.Send(13000, "127.0.0.1", message);
+            }
+        }
+    }
+}
